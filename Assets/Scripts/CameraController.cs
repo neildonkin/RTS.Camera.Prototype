aaf882b047723a7b576d7b5d986cf99fd812ff3b
Neil Donkin <neildonkin@outlook.com>
2022-01-08 12:29:25 +0000

    Followed best practice by handling input in the LateUpdate method, which is invoked after all other game object positions have been calculdated in the Update method

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -38,7 +38,7 @@ public class CameraController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         HandleMouseInput();
         HandleMovementInput();
